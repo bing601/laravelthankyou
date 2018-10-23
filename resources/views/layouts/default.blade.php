0d489374acd1cb0733e7377fde14446a414fcdaa
bing601 <JIAneite21>
2018-10-23 11:24:07 -0400

    Task: jumbotron layoud added

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,10 +6,12 @@
 <body>
 
     @include('includes.menu')
-
-    @yield('content')
-
+    <main role="main">
+        @yield('content')
+    </main>
 @include('includes.jsfooter')
+@include('includes.footer')
+
 
 </body>
 </html>
