80de814cc880c3eaf1350b0229b79ed3bfaeac2f
bing601 <JIAneite21>
2018-10-23 13:30:55 -0400

    Fix: Web Content Updated

---
+++ resources/views/pages/about.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.default')
+
+@section('content')
+
+
+    <h1>Boston Celtics</h1>
+    <p class="lead">Boston Celtics, American professional basketball team based in Boston, Massachusetts. <br> One of the most successful franchises in sports history, the Celtics won 11 of 13 National Basketball Association (NBA) championships from 1957 to 1969. Overall, they have won 17 NBA titles.</p>
+
+
+@endsection
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -5,8 +5,7 @@
         <!-- Main jumbotron for a primary marketing message or call to action -->
         <div class="jumbotron">
             <div class="container">
-                <h1 class="display-3">Hello, world!</h1>
-                <p>This is a template for a simple marketing or informational website. It includes a large callout called a jumbotron and three supporting pieces of content. Use it as a starting point to create something more unique.</p>
+                <h1 class="display-3">Celtics Legends!</h1>
                 <p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more &raquo;</a></p>
             </div>
         </div>
@@ -15,18 +14,30 @@
             <!-- Example row of columns -->
             <div class="row">
                 <div class="col-md-4">
-                    <h2>Heading</h2>
-                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                    <h2>Red Auerbach</h2>
+                    <p>Red had been a part of the Celtics family for 57 years and he had been a part of the National Basketball Association for 60 years, never missing the opening of a season. The Boston Celtics organization has dedicated the 2006-07 season in his memory.
+                        With Red's guidance and knowledge, the Celtics continued to win. In 1968 and 1969, the Celtics added two more titles. After a down period in the early 1970s, Auerbach restructured the team, adding key personnel through trades and the draft, and ultimately raised banners in 1974 and 1976. It was the subsequent years that would provide Auerbach with his biggest challenge, as the late '70s proved to be a dismal period for the Celtics. Yet Auerbach once again saved the franchise with the shrewd selection of Larry Bird in the 1978 draft. Although he would have to wait a year before Bird could play in the NBA, Auerbach sensed that the Indiana native was the man to rejuvenate the proud organization. Five other teams had a shot at Bird, but all passed.
+                        In 1981, Boston became the NBA champions again as Auerbach orchestrated another amazing trade, this time dealing the ﬁrst and 13th picks in the 1981 draft to Golden State for Robert Parish and the third overall pick (Kevin McHale). Boston added two more titles in 1984 and 1986 thanks to the master's keen additions of Danny Ainge, Dennis Johnson, Bill Walton, and others.</p>
                     <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
                 </div>
                 <div class="col-md-4">
-                    <h2>Heading</h2>
-                    <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus. Etiam porta sem malesuada magna mollis euismod. Donec sed odio dui. </p>
+                    <h2>Larry Bird</h2>
+                    <p>There are many Celtics Legends, but there is only one man whose nickname actually is Legend. Larry Bird might be the most well-known Celtics player of all time, and his incredible career led to him being dubbed with an iconic nickname: Larry Legend.
+                        His success in the NBA began immediately. Bird averaged 21.3 PPG and 10.4 RPG during his rookie season, helping to lead the Celtics to a 61-21 record in 1979-80. The 61 wins were 32 more than the Celtics accumulated during the prior season. One season later, Bird put up 21.2 PPG, 10.9 RPG and 5.5 APG and helped Boston win its first NBA championship since 1976.
+
+                        The 1981 World Championship was the first of three that Bird would win during his illustrious career. The final two, obtained in 1984 and 1986, featured Finals MVP performances by Bird. He averaged 27.4 PPG and 14.0 RPG during the 1984 Finals victory over the Los Angeles Lakers and 24.0 PPG, 9.7 RPG and 9.5 APG during the 1986 victory over the Houston Rockets.
+
+                        Bird was a fantastic postseason player, but his regular-season accomplishments were just as great. He scored more than 20.0 PPG in 11 of his 13 NBA seasons, finishing with a career average of 24.3 PPG. He also finished with an average of 10.0 RPG during his career, helping him to reach the rare accomplishment of averaging a double-double for an entire career. Perhaps most impressive of his accomplishments, Bird racked up an astounding 69 triple-doubles during his career. That number, which is far and away the most in Celtics history, showcases how great Bird was in all aspects of the game. Those skills led to him winning three consecutive MVP awards from 1984 to 1986.</p>
                     <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
                 </div>
                 <div class="col-md-4">
-                    <h2>Heading</h2>
-                    <p>Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.</p>
+                    <h2>Bill Russell</h2>
+                    <p>There are champions, and then there is Bill Russell. Russell is the champion of champions, as he owns the most NBA championship rings of any player in the history of the game, with 11, and has been winning off the court for a half-century.
+                        Russell’s dominant play around the paint changed the dynamic of the NBA for 13 seasons. He was the missing piece that the Celtics – and every other team in the league, for that matter – were missing. Russell would go on to average 15.1 PPG, 22.5 RPG and 4.3 APG during his 13-year career. He grabbed 40 rebounds in two separate NBA Finals games and is one of only two players to grab more than 50 rebounds in a single NBA game (51 rebounds on Feb. 6, 1960 vs. Syracuse).
+
+                        The level of success Russell experienced during his career was unprecedented, and it hasn’t even been approached since. He won 11 titles and made 12 All-Star teams in just 13 seasons. He also managed to win five MVP awards, and even pulled in an MVP award at the 1963 All-Star game.
+
+                        His success certainly was not limited to his playing skills, either. Russell doubled as the Celtics’ head coach for his final three seasons and won two championships during that reign. But what might be even more important than his playing and coaching achievements was his impact in the community.</p>
                     <p><a class="btn btn-secondary" href="#" role="button">View details &raquo;</a></p>
                 </div>
             </div>
